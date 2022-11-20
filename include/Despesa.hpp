4363f4e3c5b6ee9f2e4592d0b34ff8a11f7a932f
Pedro Medina <m3dina@protonmail.com>
2022-11-20 12:39:03 -0300

    Criação da classe Despesa

---
+++ include/Despesa.hpp
@@ -0,0 +1,29 @@
+#ifndef DESPESA_H
+#define DESPESA_U
+
+#include "ContaBancaria.hpp"
+#include "Transacao.hpp"
+
+#include <memory>
+
+class Despesa : Transacao {
+    private:
+        std::shared_ptr<ContaBancaria> _conta = nullptr;
+        bool _despesa_cartao_credito;
+    
+    public:
+        Despesa(double valor, std::string descricao, std::string data, std::string categoria, std::shared_ptr<ContaBancaria> conta, bool despesa_cartao_credito);
+
+        //caso não seja informado se a despesa é do cartão, será false por default
+        Despesa(double valor, std::string descricao, std::string data, std::string categoria, std::shared_ptr<ContaBancaria> conta);
+
+        std::shared_ptr<ContaBancaria> getConta();
+        void alterarConta(std::shared_ptr<ContaBancaria> novo_banco);
+
+        bool isDespesaCartaoCredito();
+        void setDespesaCartaoCredito(bool arg);
+
+};
+
+#endif
+
---
+++ src/res/Despesa.cpp
@@ -0,0 +1,18 @@
+#include "Despesa.hpp"
+
+Despesa::Despesa(double valor, std::string descricao, std::string data, std::string categoria, std::shared_ptr<ContaBancaria> conta, bool despesa_cartao_credito) : Transacao(valor, descricao, data, categoria), _conta(conta), _despesa_cartao_credito(despesa_cartao_credito) {}
+
+Despesa::Despesa(double valor, std::string descricao, std::string data, std::string categoria, std::shared_ptr<ContaBancaria> conta) : Transacao(valor, descricao, data, categoria), _conta(conta), _despesa_cartao_credito(false) {}
+
+void Despesa::alterarConta(std::shared_ptr<ContaBancaria> nova_conta) {
+    this->_conta = nova_conta;
+}
+
+bool Despesa::isDespesaCartaoCredito() {
+    return this->_despesa_cartao_credito;
+}
+
+void Despesa::setDespesaCartaoCredito(bool arg) {
+    this->_despesa_cartao_credito = arg;
+}
+
