d4558c4bb2b1fc243bdaf21fe9f923a8dd09ec98
luk3rr <luk3rr@gmail.com>
2022-11-26 15:24:38 -0300

    reimplementacao da classe despesa

--- include/Despesa.hpp
+++ include/Despesa.hpp
@@ -5,29 +5,17 @@
 
 #include <memory>
 
-class Despesa : Transacao {
+class Despesa : public Transacao {
     private:
         std::string _conta;
-        bool _despesa_cartao_credito;
-    
-    public:
-        Despesa(double valor, std::string descricao, std::string data,
-            std::string categoria, std::string conta,
-            bool despesa_cartao_credito);
 
-        // caso não seja informado se a despesa é do cartão, será false por default
-        Despesa(double valor, std::string descricao, std::string data,
-            std::string categoria, std::string conta);
+    public:
+        Despesa(double valor, std::string data, std::string categoria,
+                std::string conta);
 
         std::string getConta();
 
-        void alterarConta(std::string novo_banco);
-
-        bool isDespesaCartaoCredito();
-
-        void setDespesaCartaoCredito(bool arg);
-
+        void alterarConta(std::string nova_conta);
 };
 
 #endif
-
--- src/res/Despesa.cpp
+++ src/res/Despesa.cpp
@@ -1,13 +1,9 @@
 #include "Despesa.hpp"
 
-Despesa::Despesa(double valor, std::string descricao, std::string data,
-    std::string categoria, std::string conta, bool despesa_cartao_credito) 
-    : Transacao(valor, descricao, data, categoria), _conta(conta),
-    _despesa_cartao_credito(despesa_cartao_credito) { }
-
-Despesa::Despesa(double valor, std::string descricao, std::string data,
-    std::string categoria, std::string conta) : Transacao(valor, descricao,
-    data, categoria), _conta(conta), _despesa_cartao_credito(false) { }
+Despesa::Despesa(double valor, std::string data, std::string categoria,
+                 std::string conta) : Transacao(valor, data, categoria) {
+    this->_conta = conta;
+}
 
 std::string Despesa::getConta() {
     return this->_conta;
@@ -16,12 +12,3 @@ std::string Despesa::getConta() {
 void Despesa::alterarConta(std::string nova_conta) {
     this->_conta = nova_conta;
 }
-
-bool Despesa::isDespesaCartaoCredito() {
-    return this->_despesa_cartao_credito;
-}
-
-void Despesa::setDespesaCartaoCredito(bool arg) {
-    this->_despesa_cartao_credito = arg;
-}
-
