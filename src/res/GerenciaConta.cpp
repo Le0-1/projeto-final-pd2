4e5dd86b782fe52c769dfe0d3f4a1112e1b50f07
Pedro Medina <m3dina@protonmail.com>
2022-11-27 14:51:47 -0300

    Refatoração GerenciaConta make_shared, nomenclatura e indentação

--- src/res/GerenciaConta.cpp
+++ src/res/GerenciaConta.cpp
@@ -20,14 +20,14 @@ void GerenciaConta::adicionarCarteira(std::string nome, double saldo_inicial) {
         throw gcexcp::SaldoInvalido(saldo_inicial, nome);
     }
 
-    std::shared_ptr<Carteira> carteira(new Carteira(nome, saldo_inicial));
+    std::shared_ptr<Carteira> carteira = std::make_shared<Carteira>(nome, saldo_inicial);
     getContas().insert(std::pair<std::string, std::shared_ptr<Carteira>>(nome, carteira));
 }
 
 void GerenciaConta::adicionarConta(std::string nome, double saldo_inicial) {
-    std::shared_ptr<ContaBancaria> carteira(new ContaBancaria(nome, saldo_inicial));
-    getContas().insert(std::pair<std::string, std::shared_ptr<Carteira>>(nome,
-        carteira));
+    
+    std::shared_ptr<ContaBancaria> conta = std::make_shared<ContaBancaria>(nome, saldo_inicial);
+    getContas().insert(std::pair<std::string, std::shared_ptr<Carteira>>(nome, conta));
 }
 
 void GerenciaConta::removerConta(std::string nome) {
@@ -35,8 +35,9 @@ void GerenciaConta::removerConta(std::string nome) {
 }
 
 void GerenciaConta::adicionarReceita(std::string conta, double valor,
-    std::string data, std::string categoria) {
-    double saldo_conta =  getConta(conta)->getSaldoAtual();
+                                     std::string data, std::string categoria) {
+
+    double saldo_conta = getConta(conta)->getSaldoAtual();
     Receita receita(conta, valor, data, categoria);
 
     getConta(conta)->setSaldoAtual(saldo_conta + valor);
@@ -44,7 +45,8 @@ void GerenciaConta::adicionarReceita(std::string conta, double valor,
 }
 
 void GerenciaConta::adicionarDespesa(std::string conta, double valor,
-    std::string data,std::string categoria) {
+                                     std::string data, std::string categoria) {
+
     double saldo_conta = getConta(conta)->getSaldoAtual();
     Despesa despesa(valor, data, categoria, conta);
 
@@ -53,24 +55,28 @@ void GerenciaConta::adicionarDespesa(std::string conta, double valor,
 }
 
 void GerenciaConta::adicionarDespesaCartao(std::string conta, std::string cartao,
-    double valor, std::string data, std::string categoria) {
+                                           double valor, std::string data,
+                                           std::string categoria) {
+
     double saldo_conta = getConta(conta)->getSaldoAtual();
     Despesa despesa(valor, data, categoria, conta);
 
     getConta(conta)->setSaldoAtual(saldo_conta - valor);
+
     if (getConta(conta)->getSubtipo() == "ContaBancaria") {
-        std::shared_ptr<ContaBancaria> contaBancaria;
-        contaBancaria = std::dynamic_pointer_cast<ContaBancaria>(getConta(conta));
-        contaBancaria->getCartaoDeCredito(cartao).adicionarDespesa(valor, data, 
-        categoria);
+        std::shared_ptr<ContaBancaria> conta_bancaria;
+        conta_bancaria = std::dynamic_pointer_cast<ContaBancaria>(getConta(conta));
+        conta_bancaria->getCartaoDeCredito(cartao).adicionarDespesa(valor, data, categoria);
     }
 }
 
-void GerenciaConta::adicionarTransferencia(double valor, std::string data,
-    std::string categoria, std::string origem, std::string destino) { 
+void GerenciaConta::adicionarTransferencia(double valor, std::string data, 
+                                           std::string categoria, std::string origem,
+                                           std::string destino) {
+
     Transferencia transferencia(valor, data, categoria, origem, destino);
-    getTransferencias().insert(std::pair<int, Transferencia>
-        (transferencia.getID(), transferencia));
+    getTransferencias().insert(std::pair<int, Transferencia>(transferencia.getID(),
+                                                             transferencia));
 
     double saldo_conta_origem = getConta(origem)->getSaldoAtual();
     double saldo_conta_destino = getConta(destino)->getSaldoAtual();
@@ -88,10 +94,11 @@ void GerenciaConta::removerDespesa(std::string conta, int id) {
 }
 
 void GerenciaConta::removerDespesaCartao(std::string conta, std::string cartao, int id) {
+
     if (getConta(conta)->getSubtipo() == "ContaBancaria") {
-        std::shared_ptr<ContaBancaria> contaBancaria;
-        contaBancaria = std::dynamic_pointer_cast<ContaBancaria>(getConta(conta));
-        contaBancaria->getCartoes().find(cartao)->second.removerDespesa(id);
+        std::shared_ptr<ContaBancaria> conta_bancaria;
+        conta_bancaria = std::dynamic_pointer_cast<ContaBancaria>(getConta(conta));
+        conta_bancaria->getCartoes().find(cartao)->second.removerDespesa(id);
     }
 }
 
@@ -100,48 +107,51 @@ void GerenciaConta::removerTransferencia(int id) {
 }
 
 void GerenciaConta::adicionarCartao(std::string conta, std::string nome,
-    std::string numero, std::string CVV, std::string fechamento,
-    double limite_cartao) {
+                                    std::string numero, std::string CVV,
+                                    std::string fechamento, double limite_cartao) {
+
     if (getConta(conta)->getSubtipo() == "ContaBancaria") {
-        std::shared_ptr<ContaBancaria> contaBancaria;
-        contaBancaria = std::dynamic_pointer_cast<ContaBancaria>(getConta(conta));
+        std::shared_ptr<ContaBancaria> conta_bancaria;
+        conta_bancaria = std::dynamic_pointer_cast<ContaBancaria>(getConta(conta));
 
-        CartaoDeCredito cartaoDeCredito(nome, numero, CVV, fechamento, limite_cartao);
+        CartaoDeCredito cartao_de_credito(nome, numero, CVV, fechamento, limite_cartao);
 
-        contaBancaria->adicionarCartao(cartaoDeCredito);
+        conta_bancaria->adicionarCartao(cartao_de_credito);
     }
 }
 void GerenciaConta::removerCartao(std::string conta, std::string cartao) {
-    if (getConta(conta)->getSubtipo() == "ContaBancaria") {
-        std::shared_ptr<ContaBancaria> contaBancaria;
-        contaBancaria = std::dynamic_pointer_cast<ContaBancaria>(getConta(conta));
 
-        contaBancaria->removerCartao(cartao);
+    if (getConta(conta)->getSubtipo() == "ContaBancaria") {
+        std::shared_ptr<ContaBancaria> conta_bancaria;
+        conta_bancaria = std::dynamic_pointer_cast<ContaBancaria>(getConta(conta));
+
+        conta_bancaria->removerCartao(cartao);
     }
 }
 
 void GerenciaConta::pagarFatura(std::string conta, std::string cartao) {
+
     if (getConta(conta)->getSubtipo() == "ContaBancaria") {
-        std::shared_ptr<ContaBancaria> contaBancaria;
-        contaBancaria = std::dynamic_pointer_cast<ContaBancaria>(getConta(conta));
+        std::shared_ptr<ContaBancaria> conta_bancaria;
+        conta_bancaria = std::dynamic_pointer_cast<ContaBancaria>(getConta(conta));
 
-        CartaoDeCredito cartaoDeCredito = contaBancaria->getCartaoDeCredito(cartao);
+        CartaoDeCredito cartao_de_credito = conta_bancaria->getCartaoDeCredito(cartao);
 
-        std::list<std::shared_ptr<Despesa>> listaDespesa;
-        listaDespesa = cartaoDeCredito.getListaDespesas();
+        std::list<std::shared_ptr<Despesa>> lista_despesas;
+        lista_despesas = cartao_de_credito.getListaDespesas();
 
-        for (std::shared_ptr<Despesa> despesa : listaDespesa) {
+        for (std::shared_ptr<Despesa> despesa : lista_despesas) {
             adicionarDespesa(conta, despesa->getValor(), despesa->getData(),
-                despesa->getCategoria());
+                             despesa->getCategoria());
         }
-        listaDespesa.clear();
+        lista_despesas.clear();
     }
 }
 
 void GerenciaConta::imprimirContas() {
     std::cout << "===============================================" << std::endl;
 
-    for (auto &conta : getContas()) {
+    for (auto const& conta : getContas()) {
         std::cout << conta.second->getNome() << std::endl;
     }
     std::cout << "===============================================" << std::endl;
